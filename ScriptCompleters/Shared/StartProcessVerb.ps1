7a2b9fb7cc725022880cc992637105ccd59671a0
MartinGC94 <mgc94@outlook.dk>
2022-09-02 18:59:29 +0200

    Add verb completer for start-process

---
+++ ScriptCompleters/Shared/StartProcessVerb.ps1
@@ -0,0 +1,30 @@
+﻿using module .\Classes\CompletionHelper.psm1
+using namespace System
+using namespace System.Management.Automation
+
+Register-ArgumentCompleter -CommandName Start-Process -ParameterName Verb -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $TrimmedWord = [CompletionHelper]::TrimQuotes($wordToComplete)
+
+    $FilePath = $fakeBoundParameters['FilePath']
+    if ([string]::IsNullOrWhiteSpace($FilePath))
+    {
+        return
+    }
+
+    $Executable = [CompletionHelper]::GetCachedResults("Get-Command -Name '$FilePath' -CommandType Application -ErrorAction Ignore", $true)
+    if ($null -eq $Executable)
+    {
+        return
+    }
+
+    $CommandInfo = [CompletionHelper]::GetCachedResults("[System.Diagnostics.ProcessStartInfo]::new('A$($Executable.Extension)')", $false)
+
+    foreach ($Verb in $CommandInfo.Verbs)
+    {
+        if ($Verb.StartsWith($TrimmedWord, [StringComparison]::OrdinalIgnoreCase))
+        {
+            [CompletionHelper]::NewParamCompletionResult($Verb)
+        }
+    }
+}
