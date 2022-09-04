e82d6b465f3a43b257af02a077a34af2035b1c5a
MartinGC94 <mgc94@outlook.dk>
2022-09-04 12:47:21 +0200

    Add process, port and address completers for Get-NetTCPConnection

---
+++ ScriptCompleters/Shared/GetNetTcpConnectionPortAndAddress.ps1
@@ -0,0 +1,40 @@
+﻿using module .\Classes\CompletionHelper.psm1
+
+$ScriptBlock = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $TrimmedWord = [CompletionHelper]::TrimQuotes($wordToComplete)
+    $Values = switch ($parameterName)
+    {
+        'LocalAddress'
+        {
+            Get-NetTCPConnection -LocalAddress "$TrimmedWord*" | Select-Object -ExpandProperty LocalAddress | Sort-Object -Unique
+            break
+        }
+        'RemoteAddress'
+        {
+            Get-NetTCPConnection -RemoteAddress "$TrimmedWord*" | Select-Object -ExpandProperty RemoteAddress | Sort-Object -Unique
+            break
+        }
+        'LocalPort'
+        {
+            Get-NetTCPConnection | Select-Object -ExpandProperty LocalPort | Sort-Object -Unique
+            break
+        }
+        'RemotePort'
+        {
+            Get-NetTCPConnection | Select-Object -ExpandProperty LocalAddress | Sort-Object -Unique
+            break
+        }
+    }
+    foreach ($Value in $Values)
+    {
+        if ($Value -like "$wordToComplete*")
+        {
+            [CompletionHelper]::NewParamCompletionResult($Value)
+        }
+    }
+}
+Register-ArgumentCompleter -ScriptBlock $ScriptBlock -CommandName Get-NetTCPConnection -ParameterName LocalAddress
+Register-ArgumentCompleter -ScriptBlock $ScriptBlock -CommandName Get-NetTCPConnection -ParameterName RemoteAddress
+Register-ArgumentCompleter -ScriptBlock $ScriptBlock -CommandName Get-NetTCPConnection -ParameterName LocalPort
+Register-ArgumentCompleter -ScriptBlock $ScriptBlock -CommandName Get-NetTCPConnection -ParameterName RemotePort
---
+++ ScriptCompleters/Shared/GetNetTcpConnectionProcess.ps1
@@ -0,0 +1,22 @@
+﻿using module .\Classes\CompletionHelper.psm1
+using namespace System
+using namespace System.Management.Automation
+
+Register-ArgumentCompleter -CommandName Get-NetTCPConnection -ParameterName OwningProcess -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $TrimmedWord = [CompletionHelper]::TrimQuotes($wordToComplete)
+
+    foreach ($Process in Get-Process -Name "$TrimmedWord*")
+    {
+        $ListItem = "$($Process.Name) $($Process.Id)"
+        if ($ListItem.StartsWith($TrimmedWord, [StringComparison]::OrdinalIgnoreCase))
+        {
+            [CompletionResult]::new(
+                $Process.Id,
+                $ListItem,
+                [CompletionResultType]::ParameterValue,
+                $ListItem
+            )
+        }
+    }
+}
